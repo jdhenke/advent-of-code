e877ea12b5ee9fd80c09699357ae539ff6d97480
Joe Henke <joed.henke@gmail.com>
2022-01-07 14:46:30 -0500

    Fix add-day script

--- add-day/main.go
+++ add-day/main.go
@@ -9,16 +9,23 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
 	"strings"
 )
 
 func main() {
+	year := flag.Int("year", 0, "year to add")
 	day := flag.Int("day", 0, "day to add")
 	flag.Parse()
+	if *year == 0 {
+		log.Fatal("Must provide -year flag.")
+	}
 	if *day == 0 {
 		log.Fatal("Must provide -day flag.")
 	}
-	if err := createDay(*day); err != nil {
+	if err := createDay(*year, *day); err != nil {
 		log.Fatal(err)
 	}
 	log.Println("Success")
@@ -31,10 +38,14 @@ import (
 )
 
 func Part1(r io.Reader) (answer int, err error) {
-	return 0, nil
+	return day$DAY(r)
 }
 
 func Part2(r io.Reader) (answer int, err error) {
+	return day$DAY(r)
+}
+
+func day$DAY(r io.Reader) (answer int, err error) {
 	return 0, nil
 }
 `
@@ -42,42 +53,53 @@ func Part2(r io.Reader) (answer int, err error) {
 const testFile = `package day$DAY_test
 
 import (
-	"advent-of-code/2021/day$DAY"
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
-	"strings"
+	"advent-of-code/$YEAR/day$DAY"
+	"advent-of-code/tester"
 	"testing"
 )
 
 var testData = ""
 
 func TestPart1(t *testing.T) {
-	ans, err := day$DAY.Part1(strings.NewReader(testData))
-	require.NoError(t, err)
-	assert.Equal(t, 0, ans)
+	tester.New(t, day$DAY.Part1).Run(
+		tester.FromString(testData).Want(0),
+		tester.FromFile("input.txt").Want(0),
+	)
 }
 
 func TestPart2(t *testing.T) {
-	ans, err := day$DAY.Part2(strings.NewReader(testData))
-	require.NoError(t, err)
-	assert.Equal(t, 0, ans)
+	tester.New(t, day$DAY.Part2).Run(
+		tester.FromString(testData).Want(0),
+		tester.FromFile("input.txt").Want(0),
+	)
 }
 `
 
-const solverLine = `		{$DAY, 1}: day$DAY.Part1,
-		{$DAY, 2}: day$DAY.Part2,
-`
+const solverLine = `		{$YEAR, $DAY, 1}: aoc$YEARday$DAY.Part1,
+		{$YEAR, $DAY, 2}: aoc$YEARday$DAY.Part2,`
 
-func createDay(day int) error {
-	dir := fmt.Sprintf("day%d", day)
+func sub(data string, year, day int) string {
+	return strings.Replace(
+		strings.Replace(data, `$DAY`, fmt.Sprint(day), -1),
+		`$YEAR`,
+		fmt.Sprint(year),
+		-1,
+	)
+}
+
+func createDay(year, day int) error {
+	if err := os.MkdirAll(fmt.Sprint(year), 0755); err != nil {
+		return err
+	}
+	dir := filepath.Join(fmt.Sprint(year), fmt.Sprintf("day%d", day))
 	if err := os.Mkdir(dir, 0755); err != nil {
 		return err
 	}
-	f := strings.Replace(libFile, `$DAY`, fmt.Sprint(day), -1)
+	f := sub(libFile, year, day)
 	if err := ioutil.WriteFile(filepath.Join(dir, fmt.Sprintf("day%d.go", day)), []byte(f), 0644); err != nil {
 		return err
 	}
-	tf := strings.Replace(testFile, `$DAY`, fmt.Sprint(day), -1)
+	tf := sub(testFile, year, day)
 	if err := ioutil.WriteFile(filepath.Join(dir, fmt.Sprintf("day%d_test.go", day)), []byte(tf), 0644); err != nil {
 		return err
 	}
@@ -85,30 +107,60 @@ func createDay(day int) error {
 	if err != nil {
 		return err
 	}
-	sl := strings.Replace(solverLine, `$DAY`, fmt.Sprint(day), -1)
+	sl := sub(solverLine, year, day)
 	var buf bytes.Buffer
 	var inSolvers bool
+	var solverLines = strings.Split(sl, "\n")
 	for _, line := range strings.Split(string(mainBytes), "\n") {
-		if strings.HasPrefix(line, `	solvers :=`) {
-			inSolvers = true
-		}
 		if inSolvers && strings.HasPrefix(line, `	}`) {
-			_, _ = fmt.Fprint(&buf, sl)
+			sort.Slice(solverLines, func(i, j int) bool {
+				return solverNum(solverLines[i]) < solverNum(solverLines[j])
+			})
+			for _, l := range solverLines {
+				_, _ = fmt.Fprintln(&buf, l)
+			}
 			inSolvers = false
+		}
+		if inSolvers {
+			solverLines = append(solverLines, line)
+			continue
+		}
+		if strings.HasPrefix(line, `	solutions :=`) {
+			inSolvers = true
 		}
 		_, _ = fmt.Fprintln(&buf, line)
 		if strings.HasPrefix(line, `import (`) {
-			_, _ = fmt.Fprintln(&buf, fmt.Sprintf(`"advent-of-code/2021/day%d"`, day))
+			_, _ = fmt.Fprintln(&buf, fmt.Sprintf("\taoc%dday%d \"advent-of-code/%d/day%d\"", year, day, year, day))
 		}
 	}
+	_, _ = buf.Write([]byte("\n"))
 	if err := ioutil.WriteFile("main.go", buf.Bytes(), 0644); err != nil {
 		return err
 	}
-	if err := ioutil.WriteFile(filepath.Join("data", fmt.Sprintf("day%d.txt", day)), []byte{}, 0644); err != nil {
+	if err := ioutil.WriteFile(filepath.Join(fmt.Sprint(year), fmt.Sprintf("day%d", day), "input.txt"), []byte{}, 0644); err != nil {
 		return err
 	}
-	if err := exec.Command("go", "fmt", "./...").Run(); err != nil {
+	if err := exec.Command("./godelw", "format").Run(); err != nil {
 		return err
 	}
 	return nil
+}
+
+// {2021, 1, 1}:  aoc2021day1.Part1,
+var re = regexp.MustCompile(`{(\d+), (\d+), (\d+)}:.*,`)
+
+func solverNum(line string) int {
+	parts := re.FindStringSubmatch(line)
+	if parts == nil {
+		panic("bad solver line")
+	}
+	return num(parts[1])*1000 + num(parts[2])*10 + num(parts[3])
+}
+
+func num(s string) int {
+	d, err := strconv.Atoi(s)
+	if err != nil {
+		panic(err)
+	}
+	return d
 }

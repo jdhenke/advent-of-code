c66de58eaaca0268c22ade6aba93807d1a06017f
Joe Henke <joed.henke@gmail.com>
2022-01-07 07:25:30 -0500

    Initial commit

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	var (
+		year, day, part int
+		file            string
+	)
+	flag.IntVar(&year, "year", 0, "year")
+	flag.IntVar(&day, "day", 0, "day")
+	flag.IntVar(&part, "part", 0, "part")
+	flag.StringVar(&file, "file", "", "file")
+	flag.Parse()
+	if year == 0 {
+		log.Fatal("Error: -year required.")
+	}
+	if day == 0 {
+		log.Fatal("Error: -day required.")
+	}
+	if part == 0 {
+		log.Fatal("Error: -part required.")
+	}
+	if file == "" {
+		log.Fatal("Error: -file required.")
+	}
+	solution, ok := getSolution(year, day, part)
+	if !ok {
+		log.Fatalf("Error: No solution present for %v %v %v.", year, day, part)
+	}
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatalf("Error opening file: %v", err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Printf("Error closing file: %v", err)
+		}
+	}()
+	ans, err := solution(f)
+	if err != nil {
+		log.Fatalf("Error running solution: %v", err)
+	}
+	fmt.Println(ans)
+}
+
+type Solution func(r io.Reader) (ans int, err error)
+
+type key struct {
+	year, day, part int
+}
+
+var solutions = make(map[key]Solution)
+
+func getSolution(year, day, part int) (Solution, bool) {
+	sol, ok := solutions[key{year, day, part}]
+	return sol, ok
+}

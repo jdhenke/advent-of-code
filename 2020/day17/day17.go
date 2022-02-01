859772054d958123e1ac0a42e327a3ab97438aa3
Joe Henke <joed.henke@gmail.com>
2022-02-01 15:26:47 -0700

    2020 day 17 part 2

--- 2020/day17/day17.go
+++ 2020/day17/day17.go
@@ -177,20 +177,288 @@ Starting with your given initial configuration, simulate six cycles. How many
 cubes are left in the active state after the sixth cycle?
 */
 func Part1(r io.Reader) (answer int, err error) {
-	return day17(r)
+	return day17(r, 3)
 }
 
+/*
+Part2 Prompt
+
+--- Part Two ---
+For some reason, your simulated results don't match what the experimental
+energy source engineers expected. Apparently, the pocket dimension actually has
+four spatial dimensions, not three.
+
+The pocket dimension contains an infinite 4-dimensional grid. At every integer
+4-dimensional coordinate (x,y,z,w), there exists a single cube (really, a
+hypercube) which is still either active or inactive.
+
+Each cube only ever considers its neighbors: any of the 80 other cubes where
+any of their coordinates differ by at most 1. For example, given the cube at
+x=1,y=2,z=3,w=4, its neighbors include the cube at x=2,y=2,z=3,w=3, the cube at
+x=0,y=2,z=3,w=4, and so on.
+
+The initial state of the pocket dimension still consists of a small flat region
+of cubes. Furthermore, the same rules for cycle updating still apply: during
+each cycle, consider the number of active neighbors of each cube.
+
+For example, consider the same initial state as in the example above. Even
+though the pocket dimension is 4-dimensional, this initial state represents a
+small 2-dimensional slice of it. (In particular, this initial state defines a
+3x3x1x1 region of the 4-dimensional space.)
+
+Simulating a few cycles from this initial state produces the following
+configurations, where the result of each cycle is shown layer-by-layer at each
+given z and w coordinate:
+
+    Before any cycles:
+
+    z=0, w=0
+    .#.
+    ..#
+    ###
+
+    After 1 cycle:
+
+    z=-1, w=-1
+    #..
+    ..#
+    .#.
+
+    z=0, w=-1
+    #..
+    ..#
+    .#.
+
+    z=1, w=-1
+    #..
+    ..#
+    .#.
+
+    z=-1, w=0
+    #..
+    ..#
+    .#.
+
+    z=0, w=0
+    #.#
+    .##
+    .#.
+
+    z=1, w=0
+    #..
+    ..#
+    .#.
+
+    z=-1, w=1
+    #..
+    ..#
+    .#.
+
+    z=0, w=1
+    #..
+    ..#
+    .#.
+
+    z=1, w=1
+    #..
+    ..#
+    .#.
+
+    After 2 cycles:
+
+    z=-2, w=-2
+    .....
+    .....
+    ..#..
+    .....
+    .....
+
+    z=-1, w=-2
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=0, w=-2
+    ###..
+    ##.##
+    #...#
+    .#..#
+    .###.
+
+    z=1, w=-2
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=2, w=-2
+    .....
+    .....
+    ..#..
+    .....
+    .....
+
+    z=-2, w=-1
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=-1, w=-1
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=0, w=-1
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=1, w=-1
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=2, w=-1
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=-2, w=0
+    ###..
+    ##.##
+    #...#
+    .#..#
+    .###.
+
+    z=-1, w=0
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=0, w=0
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=1, w=0
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=2, w=0
+    ###..
+    ##.##
+    #...#
+    .#..#
+    .###.
+
+    z=-2, w=1
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=-1, w=1
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=0, w=1
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=1, w=1
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=2, w=1
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=-2, w=2
+    .....
+    .....
+    ..#..
+    .....
+    .....
+
+    z=-1, w=2
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=0, w=2
+    ###..
+    ##.##
+    #...#
+    .#..#
+    .###.
+
+    z=1, w=2
+    .....
+    .....
+    .....
+    .....
+    .....
+
+    z=2, w=2
+    .....
+    .....
+    ..#..
+    .....
+    .....
+
+After the full six-cycle boot process completes, 848 cubes are left in the
+active state.
+
+Starting with your given initial configuration, simulate six cycles in a
+4-dimensional space. How many cubes are left in the active state after the
+sixth cycle?
+*/
 func Part2(r io.Reader) (answer int, err error) {
-	return day17(r)
+	return day17(r, 4)
 }
 
-func day17(r io.Reader) (answer int, err error) {
-	grid := make(map[[3]int]bool)
+func day17(r io.Reader, dims int) (answer int, err error) {
+	grid := make(map[[4]int]bool)
 	i := 0
 	if err := input.ForEachLine(r, func(line string) error {
 		for j, s := range line {
 			if s == '#' {
-				grid[[3]int{i, j, 0}] = true
+				grid[[4]int{i, j, 0, 0}] = true
 			}
 		}
 		i++
@@ -199,20 +467,20 @@ func day17(r io.Reader) (answer int, err error) {
 		return 0, err
 	}
 	for step := 0; step < 6; step++ {
-		changes := make(map[[3]int]bool)
-		checked := make(map[[3]int]bool)
+		changes := make(map[[4]int]bool)
+		checked := make(map[[4]int]bool)
 		for x := range grid {
-			if n := activeNeighbors(grid, x); n != 2 && n != 3 {
+			if n := activeNeighbors(grid, dims, x); n != 2 && n != 3 {
 				changes[x] = false
 			}
-			forEachNeighbor(grid, x, func(y [3]int) {
+			forEachNeighbor(grid, dims, x, func(y [4]int) {
 				if grid[y] {
 					return
 				}
 				if _, ok := checked[y]; ok {
 					return
 				}
-				if n := activeNeighbors(grid, y); n == 3 {
+				if n := activeNeighbors(grid, dims, y); n == 3 {
 					changes[y] = true
 				}
 				checked[y] = true
@@ -229,9 +497,9 @@ func day17(r io.Reader) (answer int, err error) {
 	return len(grid), nil
 }
 
-func activeNeighbors(grid map[[3]int]bool, x [3]int) int {
+func activeNeighbors(grid map[[4]int]bool, dims int, x [4]int) int {
 	active := 0
-	forEachNeighbor(grid, x, func(y [3]int) {
+	forEachNeighbor(grid, dims, x, func(y [4]int) {
 		if grid[y] {
 			active++
 		}
@@ -239,14 +507,23 @@ func activeNeighbors(grid map[[3]int]bool, x [3]int) int {
 	return active
 }
 
-func forEachNeighbor(grid map[[3]int]bool, x [3]int, f func(y [3]int)) {
+func forEachNeighbor(grid map[[4]int]bool, dims int, x [4]int, f func(y [4]int)) {
 	for i := -1; i <= 1; i++ {
 		for j := -1; j <= 1; j++ {
 			for k := -1; k <= 1; k++ {
-				if i == 0 && j == 0 && k == 0 {
-					continue
+				if dims == 4 {
+					for l := -1; l <= 1; l++ {
+						if i == 0 && j == 0 && k == 0 && l == 0 {
+							continue
+						}
+						f([4]int{x[0] + i, x[1] + j, x[2] + k, x[3] + l})
+					}
+				} else {
+					if i == 0 && j == 0 && k == 0 {
+						continue
+					}
+					f([4]int{x[0] + i, x[1] + j, x[2] + k, 0})
 				}
-				f([3]int{x[0] + i, x[1] + j, x[2] + k})
 			}
 		}
 	}
--- 2020/day17/day17_test.go
+++ 2020/day17/day17_test.go
@@ -18,9 +18,9 @@ func TestPart1(t *testing.T) {
 	)
 }
 
-// func TestPart2(t *testing.T) {
-// 	tester.New(t, day17.Part2).Run(
-// 		tester.FromString(testData).Want(0),
-// 		tester.FromFile("input.txt").Want(0),
-// 	)
-// }
+func TestPart2(t *testing.T) {
+	tester.New(t, day17.Part2).Run(
+		tester.FromString(testData).Want(848),
+		tester.FromFile("input.txt").Want(2400),
+	)
+}

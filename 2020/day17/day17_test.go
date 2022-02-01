afe192d9f10a8b3f267623af8cf366267ed3d9ef
Joe Henke <joed.henke@gmail.com>
2022-02-01 15:18:11 -0700

    2020 day 17 part 1

---
+++ 2020/day17/day17_test.go
@@ -0,0 +1,26 @@
+package day17_test
+
+import (
+	"testing"
+
+	"github.com/jdhenke/advent-of-code/2020/day17"
+	"github.com/jdhenke/advent-of-code/tester"
+)
+
+var testData = `.#.
+..#
+###`
+
+func TestPart1(t *testing.T) {
+	tester.New(t, day17.Part1).Run(
+		tester.FromString(testData).Want(112),
+		tester.FromFile("input.txt").Want(448),
+	)
+}
+
+// func TestPart2(t *testing.T) {
+// 	tester.New(t, day17.Part2).Run(
+// 		tester.FromString(testData).Want(0),
+// 		tester.FromFile("input.txt").Want(0),
+// 	)
+// }

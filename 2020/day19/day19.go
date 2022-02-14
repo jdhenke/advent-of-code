a103b51156b8d688400ff22647c51d020a077718
Joe Henke <joed.henke@gmail.com>
2022-02-14 07:44:37 -0700

    Refactor day 19

--- 2020/day19/day19.go
+++ 2020/day19/day19.go
@@ -97,14 +97,108 @@ func Part1(r io.Reader) (answer int, err error) {
 	return day19(r)
 }
 
+/*
+Part2 Prompt
+
+--- Part Two ---
+As you look over the list of messages, you realize your matching rules aren't
+quite right. To fix them, completely replace rules 8: 42 and 11: 42 31 with the
+following:
+
+    8: 42 | 42 8
+    11: 42 31 | 42 11 31
+
+This small change has a big impact: now, the rules do contain loops, and the
+list of messages they could hypothetically match is infinite. You'll need to
+determine how these changes affect which messages are valid.
+
+Fortunately, many of the rules are unaffected by this change; it might help to
+start by looking at which rules always match the same set of values and how
+those rules (especially rules 42 and 31) are used by the new versions of rules
+8 and 11.
+
+(Remember, you only need to handle the rules you have; building a solution that
+could handle any hypothetical combination of rules would be significantly more
+difficult.)
+
+For example:
+
+    42: 9 14 | 10 1
+    9: 14 27 | 1 26
+    10: 23 14 | 28 1
+    1: "a"
+    11: 42 31
+    5: 1 14 | 15 1
+    19: 14 1 | 14 14
+    12: 24 14 | 19 1
+    16: 15 1 | 14 14
+    31: 14 17 | 1 13
+    6: 14 14 | 1 14
+    2: 1 24 | 14 4
+    0: 8 11
+    13: 14 3 | 1 12
+    15: 1 | 14
+    17: 14 2 | 1 7
+    23: 25 1 | 22 14
+    28: 16 1
+    4: 1 1
+    20: 14 14 | 1 15
+    3: 5 14 | 16 1
+    27: 1 6 | 14 18
+    14: "b"
+    21: 14 1 | 1 14
+    25: 1 1 | 1 14
+    22: 14 14
+    8: 42
+    26: 14 22 | 1 20
+    18: 15 15
+    7: 14 5 | 1 21
+    24: 14 1
+
+    abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+    bbabbbbaabaabba
+    babbbbaabbbbbabbbbbbaabaaabaaa
+    aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+    bbbbbbbaaaabbbbaaabbabaaa
+    bbbababbbbaaaaaaaabbababaaababaabab
+    ababaaaaaabaaab
+    ababaaaaabbbaba
+    baabbaaaabbaaaababbaababb
+    abbbbabbbbaaaababbbbbbaaaababb
+    aaaaabbaabaaaaababaa
+    aaaabbaaaabbaaa
+    aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+    babaaabbbaaabaababbaabababaaab
+    aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+
+Without updating rules 8 and 11, these rules only match three messages:
+bbabbbbaabaabba, ababaaaaaabaaab, and ababaaaaabbbaba.
+
+However, after updating rules 8 and 11, a total of 12 messages match:
+
+- bbabbbbaabaabba
+- babbbbaabbbbbabbbbbbaabaaabaaa
+- aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+- bbbbbbbaaaabbbbaaabbabaaa
+- bbbababbbbaaaaaaaabbababaaababaabab
+- ababaaaaaabaaab
+- ababaaaaabbbaba
+- baabbaaaabbaaaababbaababb
+- abbbbabbbbaaaababbbbbbaaaababb
+- aaaaabbaabaaaaababaa
+- aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+- aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+
+After updating rules 8 and 11, how many messages completely match rule 0?
+*/
 func Part2(r io.Reader) (answer int, err error) {
 	return day19(r)
 }
 
 type Rule interface {
-	// Matches indicates if the rule matches the given string s starting at the beginning and how many characters it
-	// matches.
-	Matches(s string) (matched bool, length int)
+	// Match calls f with the remaining suffix of s for every possible matching prefix of s that matches this rule,
+	// stopping if f return false or there are no more matches.
+	Match(s string, f func(remaining string) (ok bool))
 }
 
 func day19(r io.Reader) (answer int, err error) {
@@ -123,9 +217,13 @@ func day19(r io.Reader) (answer int, err error) {
 			rules[num] = rule
 			return nil
 		}
-		if ok, length := rules[0].Matches(line); ok && len(line) == length {
-			answer++
-		}
+		rules[0].Match(line, func(remaining string) (ok bool) {
+			if remaining == "" {
+				answer++
+				return false
+			}
+			return true
+		})
 		return nil
 	}); err != nil {
 		return 0, err
@@ -157,46 +255,56 @@ func parseRule(rules map[int]Rule, line string) (int, Rule, error) {
 	return num, orRule(orRules), nil
 }
 
-type ruleFunc func(s string) (bool, int)
+type ruleFunc func(s string, f func(remaining string) bool)
 
-func (f ruleFunc) Matches(s string) (bool, int) {
-	return f(s)
+func (r ruleFunc) Match(s string, f func(remaining string) bool) {
+	r(s, f)
 }
 
 func literalRule(l string) Rule {
-	return ruleFunc(func(s string) (bool, int) {
-		return strings.HasPrefix(s, l), len(l)
+	return ruleFunc(func(s string, f func(remaining string) bool) {
+		if strings.HasPrefix(s, l) {
+			f(s[len(l):])
+		}
 	})
 }
 
 func referenceRule(rules map[int]Rule, n int) Rule {
-	return ruleFunc(func(s string) (bool, int) {
-		return rules[n].Matches(s)
+	return ruleFunc(func(s string, f func(remaining string) bool) {
+		rules[n].Match(s, f)
 	})
 }
 
 func seqRule(rules []Rule) Rule {
-	return ruleFunc(func(s string) (bool, int) {
-		i := 0
-		for _, rule := range rules {
-			matched, length := rule.Matches(s[i:])
-			if !matched {
-				return false, 0
+	return ruleFunc(func(s string, f func(remaining string) bool) {
+		var helper func(remaining string) bool
+		r := 0
+		helper = func(remaining string) bool {
+			r++
+			if r >= len(rules) {
+				return f(remaining)
 			}
-			i += length
+			rules[r].Match(remaining, helper)
+			return true
 		}
-		return true, i
+		rules[0].Match(s, helper)
 	})
 }
 
 func orRule(rules []Rule) Rule {
-	return ruleFunc(func(s string) (bool, int) {
+	return ruleFunc(func(s string, f func(remaining string) bool) {
 		for _, rule := range rules {
-			matched, length := rule.Matches(s)
-			if matched {
-				return true, length
+			stop := false
+			rule.Match(s, func(remaining string) (ok bool) {
+				ok = f(remaining)
+				if !ok {
+					stop = true
+				}
+				return ok
+			})
+			if stop {
+				break
 			}
 		}
-		return false, 0
 	})
 }

8b9b51255d6e624a76c3e318e189b3220c32e6f2
Joe Henke <joed.henke@gmail.com>
2022-05-02 09:57:41 -0600

    2020 day 22 part 2

--- 2020/day22/day22.go
+++ 2020/day22/day22.go
@@ -1,6 +1,7 @@
 package day22
 
 import (
+	"encoding/json"
 	"io"
 	"regexp"
 	"sort"
@@ -138,16 +139,321 @@ Play the small crab in a game of Combat using the two decks you just dealt.
 What is the winning player's score?
 */
 func Part1(r io.Reader) (answer int, err error) {
-	return day22(r)
+	return day22(r, judgePart1)
 }
 
+/*
+Part2 Prompt
+
+--- Part Two ---
+You lost to the small crab! Fortunately, crabs aren't very good at recursion.
+To defend your honor as a Raft Captain, you challenge the small crab to a game
+of Recursive Combat.
+
+Recursive Combat still starts by splitting the cards into two decks (you offer
+to play with the same starting decks as before - it's only fair). Then, the
+game consists of a series of rounds with a few changes:
+
+- Before either player deals a card, if there was a previous round in this game
+that had exactly the same cards in the same order in the same players' decks,
+the game instantly ends in a win for player 1. Previous rounds from other games
+are not considered. (This prevents infinite games of Recursive Combat, which
+everyone agrees is a bad idea.)
+- Otherwise, this round's cards must be in a new configuration; the players
+begin the round by each drawing the top card of their deck as normal.
+- If both players have at least as many cards remaining in their deck as the
+value of the card they just drew, the winner of the round is determined by
+playing a new game of Recursive Combat (see below).
+- Otherwise, at least one player must not have enough cards left in their deck
+to recurse; the winner of the round is the player with the higher-value card.
+
+As in regular Combat, the winner of the round (even if they won the round by
+winning a sub-game) takes the two cards dealt at the beginning of the round and
+places them on the bottom of their own deck (again so that the winner's card is
+above the other card). Note that the winner's card might be the lower-valued of
+the two cards if they won the round due to winning a sub-game. If collecting
+cards by winning the round causes a player to have all of the cards, they win,
+and the game ends.
+
+Here is an example of a small game that would loop forever without the infinite
+game prevention rule:
+
+    Player 1:
+    43
+    19
+
+    Player 2:
+    2
+    29
+    14
+
+During a round of Recursive Combat, if both players have at least as many cards
+in their own decks as the number on the card they just dealt, the winner of the
+round is determined by recursing into a sub-game of Recursive Combat. (For
+example, if player 1 draws the 3 card, and player 2 draws the 7 card, this
+would occur if player 1 has at least 3 cards left and player 2 has at least 7
+cards left, not counting the 3 and 7 cards that were drawn.)
+
+To play a sub-game of Recursive Combat, each player creates a new deck by
+making a copy of the next cards in their deck (the quantity of cards copied is
+equal to the number on the card they drew to trigger the sub-game). During this
+sub-game, the game that triggered it is on hold and completely unaffected; no
+cards are removed from players' decks to form the sub-game. (For example, if
+player 1 drew the 3 card, their deck in the sub-game would be copies of the
+next three cards in their deck.)
+
+Here is a complete example of gameplay, where Game 1 is the primary game of
+Recursive Combat:
+
+    === Game 1 ===
+
+    -- Round 1 (Game 1) --
+    Player 1's deck: 9, 2, 6, 3, 1
+    Player 2's deck: 5, 8, 4, 7, 10
+    Player 1 plays: 9
+    Player 2 plays: 5
+    Player 1 wins round 1 of game 1!
+
+    -- Round 2 (Game 1) --
+    Player 1's deck: 2, 6, 3, 1, 9, 5
+    Player 2's deck: 8, 4, 7, 10
+    Player 1 plays: 2
+    Player 2 plays: 8
+    Player 2 wins round 2 of game 1!
+
+    -- Round 3 (Game 1) --
+    Player 1's deck: 6, 3, 1, 9, 5
+    Player 2's deck: 4, 7, 10, 8, 2
+    Player 1 plays: 6
+    Player 2 plays: 4
+    Player 1 wins round 3 of game 1!
+
+    -- Round 4 (Game 1) --
+    Player 1's deck: 3, 1, 9, 5, 6, 4
+    Player 2's deck: 7, 10, 8, 2
+    Player 1 plays: 3
+    Player 2 plays: 7
+    Player 2 wins round 4 of game 1!
+
+    -- Round 5 (Game 1) --
+    Player 1's deck: 1, 9, 5, 6, 4
+    Player 2's deck: 10, 8, 2, 7, 3
+    Player 1 plays: 1
+    Player 2 plays: 10
+    Player 2 wins round 5 of game 1!
+
+    -- Round 6 (Game 1) --
+    Player 1's deck: 9, 5, 6, 4
+    Player 2's deck: 8, 2, 7, 3, 10, 1
+    Player 1 plays: 9
+    Player 2 plays: 8
+    Player 1 wins round 6 of game 1!
+
+    -- Round 7 (Game 1) --
+    Player 1's deck: 5, 6, 4, 9, 8
+    Player 2's deck: 2, 7, 3, 10, 1
+    Player 1 plays: 5
+    Player 2 plays: 2
+    Player 1 wins round 7 of game 1!
+
+    -- Round 8 (Game 1) --
+    Player 1's deck: 6, 4, 9, 8, 5, 2
+    Player 2's deck: 7, 3, 10, 1
+    Player 1 plays: 6
+    Player 2 plays: 7
+    Player 2 wins round 8 of game 1!
+
+    -- Round 9 (Game 1) --
+    Player 1's deck: 4, 9, 8, 5, 2
+    Player 2's deck: 3, 10, 1, 7, 6
+    Player 1 plays: 4
+    Player 2 plays: 3
+    Playing a sub-game to determine the winner...
+
+    === Game 2 ===
+
+    -- Round 1 (Game 2) --
+    Player 1's deck: 9, 8, 5, 2
+    Player 2's deck: 10, 1, 7
+    Player 1 plays: 9
+    Player 2 plays: 10
+    Player 2 wins round 1 of game 2!
+
+    -- Round 2 (Game 2) --
+    Player 1's deck: 8, 5, 2
+    Player 2's deck: 1, 7, 10, 9
+    Player 1 plays: 8
+    Player 2 plays: 1
+    Player 1 wins round 2 of game 2!
+
+    -- Round 3 (Game 2) --
+    Player 1's deck: 5, 2, 8, 1
+    Player 2's deck: 7, 10, 9
+    Player 1 plays: 5
+    Player 2 plays: 7
+    Player 2 wins round 3 of game 2!
+
+    -- Round 4 (Game 2) --
+    Player 1's deck: 2, 8, 1
+    Player 2's deck: 10, 9, 7, 5
+    Player 1 plays: 2
+    Player 2 plays: 10
+    Player 2 wins round 4 of game 2!
+
+    -- Round 5 (Game 2) --
+    Player 1's deck: 8, 1
+    Player 2's deck: 9, 7, 5, 10, 2
+    Player 1 plays: 8
+    Player 2 plays: 9
+    Player 2 wins round 5 of game 2!
+
+    -- Round 6 (Game 2) --
+    Player 1's deck: 1
+    Player 2's deck: 7, 5, 10, 2, 9, 8
+    Player 1 plays: 1
+    Player 2 plays: 7
+    Player 2 wins round 6 of game 2!
+    The winner of game 2 is player 2!
+
+    ...anyway, back to game 1.
+    Player 2 wins round 9 of game 1!
+
+    -- Round 10 (Game 1) --
+    Player 1's deck: 9, 8, 5, 2
+    Player 2's deck: 10, 1, 7, 6, 3, 4
+    Player 1 plays: 9
+    Player 2 plays: 10
+    Player 2 wins round 10 of game 1!
+
+    -- Round 11 (Game 1) --
+    Player 1's deck: 8, 5, 2
+    Player 2's deck: 1, 7, 6, 3, 4, 10, 9
+    Player 1 plays: 8
+    Player 2 plays: 1
+    Player 1 wins round 11 of game 1!
+
+    -- Round 12 (Game 1) --
+    Player 1's deck: 5, 2, 8, 1
+    Player 2's deck: 7, 6, 3, 4, 10, 9
+    Player 1 plays: 5
+    Player 2 plays: 7
+    Player 2 wins round 12 of game 1!
+
+    -- Round 13 (Game 1) --
+    Player 1's deck: 2, 8, 1
+    Player 2's deck: 6, 3, 4, 10, 9, 7, 5
+    Player 1 plays: 2
+    Player 2 plays: 6
+    Playing a sub-game to determine the winner...
+
+    === Game 3 ===
+
+    -- Round 1 (Game 3) --
+    Player 1's deck: 8, 1
+    Player 2's deck: 3, 4, 10, 9, 7, 5
+    Player 1 plays: 8
+    Player 2 plays: 3
+    Player 1 wins round 1 of game 3!
+
+    -- Round 2 (Game 3) --
+    Player 1's deck: 1, 8, 3
+    Player 2's deck: 4, 10, 9, 7, 5
+    Player 1 plays: 1
+    Player 2 plays: 4
+    Playing a sub-game to determine the winner...
+
+    === Game 4 ===
+
+    -- Round 1 (Game 4) --
+    Player 1's deck: 8
+    Player 2's deck: 10, 9, 7, 5
+    Player 1 plays: 8
+    Player 2 plays: 10
+    Player 2 wins round 1 of game 4!
+    The winner of game 4 is player 2!
+
+    ...anyway, back to game 3.
+    Player 2 wins round 2 of game 3!
+
+    -- Round 3 (Game 3) --
+    Player 1's deck: 8, 3
+    Player 2's deck: 10, 9, 7, 5, 4, 1
+    Player 1 plays: 8
+    Player 2 plays: 10
+    Player 2 wins round 3 of game 3!
+
+    -- Round 4 (Game 3) --
+    Player 1's deck: 3
+    Player 2's deck: 9, 7, 5, 4, 1, 10, 8
+    Player 1 plays: 3
+    Player 2 plays: 9
+    Player 2 wins round 4 of game 3!
+    The winner of game 3 is player 2!
+
+    ...anyway, back to game 1.
+    Player 2 wins round 13 of game 1!
+
+    -- Round 14 (Game 1) --
+    Player 1's deck: 8, 1
+    Player 2's deck: 3, 4, 10, 9, 7, 5, 6, 2
+    Player 1 plays: 8
+    Player 2 plays: 3
+    Player 1 wins round 14 of game 1!
+
+    -- Round 15 (Game 1) --
+    Player 1's deck: 1, 8, 3
+    Player 2's deck: 4, 10, 9, 7, 5, 6, 2
+    Player 1 plays: 1
+    Player 2 plays: 4
+    Playing a sub-game to determine the winner...
+
+    === Game 5 ===
+
+    -- Round 1 (Game 5) --
+    Player 1's deck: 8
+    Player 2's deck: 10, 9, 7, 5
+    Player 1 plays: 8
+    Player 2 plays: 10
+    Player 2 wins round 1 of game 5!
+    The winner of game 5 is player 2!
+
+    ...anyway, back to game 1.
+    Player 2 wins round 15 of game 1!
+
+    -- Round 16 (Game 1) --
+    Player 1's deck: 8, 3
+    Player 2's deck: 10, 9, 7, 5, 6, 2, 4, 1
+    Player 1 plays: 8
+    Player 2 plays: 10
+    Player 2 wins round 16 of game 1!
+
+    -- Round 17 (Game 1) --
+    Player 1's deck: 3
+    Player 2's deck: 9, 7, 5, 6, 2, 4, 1, 10, 8
+    Player 1 plays: 3
+    Player 2 plays: 9
+    Player 2 wins round 17 of game 1!
+    The winner of game 1 is player 2!
+
+    == Post-game results ==
+    Player 1's deck:
+    Player 2's deck: 7, 5, 6, 2, 4, 1, 10, 8, 9, 3
+
+After the game, the winning player's score is calculated from the cards they
+have in their original deck using the same rules as regular Combat. In the
+above game, the winning player's score is 291.
+
+Defend your honor as Raft Captain by playing the small crab in a game of
+Recursive Combat using the same two decks as before. What is the winning
+player's score?
+*/
 func Part2(r io.Reader) (answer int, err error) {
-	return day22(r)
+	return day22(r, judgePart2)
 }
 
 var re = regexp.MustCompile(`Player (\d+):`)
 
-func day22(r io.Reader) (answer int, err error) {
+func day22(r io.Reader, judge judgeFunc) (answer int, err error) {
 	// parse cards
 	hands := make(map[string][]int)
 	{
@@ -171,39 +477,85 @@ func day22(r io.Reader) (answer int, err error) {
 		}
 	}
 
-	// play
+	// play and return the score of the winning hand
+	_, score := play(hands, judge)
+	return score, nil
+}
+
+type judgeFunc func(round map[string]int, hands map[string][]int) (winner string)
+
+func play(hands map[string][]int, judge judgeFunc) (winnerID string, winnerScore int) {
+	seen := make(map[string]struct{})
 	for {
+		// protect against infinite games
+		b, _ := json.Marshal(hands)
+		s := string(b)
+		if _, ok := seen[s]; ok {
+			return "1", calculateScore(hands["1"])
+		}
+		seen[s] = struct{}{}
+
+		// draw one card from each hand
 		round := make(map[string]int)
+		var cards []int
 		for player, hand := range hands {
 			if len(hand) == 0 {
 				continue
 			}
-			round[player] = hand[0]
+			card := hand[0]
+			round[player] = card
+			cards = append(cards, card)
 			hands[player] = hand[1:]
 		}
-		winner, cards := judge(round)
+		winner := judge(round, hands)
+
+		// ensure the winning card is on top and add to the winner's hand
+		sort.Slice(cards, func(i, j int) bool {
+			if cards[i] == round[winner] {
+				return true
+			}
+			return false
+		})
 		hands[winner] = append(hands[winner], cards...)
+
+		// check for a winner
 		if winningHand, over := isOver(hands); over {
-			return score(winningHand), nil
+			return winner, calculateScore(winningHand)
 		}
 	}
 }
 
-// returns cards in descending sorted order
-func judge(round map[string]int) (winner string, cards []int) {
+func judgePart1(round map[string]int, hands map[string][]int) (winner string) {
 	var maxCard int
 	for player, card := range round {
-		cards = append(cards, card)
 		if card > maxCard {
 			maxCard = card
 			winner = player
 		}
 	}
-	// reverse sort
-	sort.Slice(cards, func(i, j int) bool {
-		return cards[i] > cards[j]
-	})
-	return winner, cards
+	return winner
+}
+
+func judgePart2(round map[string]int, hands map[string][]int) (winner string) {
+	recurse := true
+	for player, card := range round {
+		if len(hands[player]) < card {
+			recurse = false
+			break
+		}
+	}
+	if recurse {
+		handsCopy := make(map[string][]int)
+		for player, hand := range hands {
+			numCardsToUse := round[player] // dictated by the card drawn by the player this round
+			handCopy := make([]int, numCardsToUse)
+			copy(handCopy, hand[:numCardsToUse])
+			handsCopy[player] = handCopy
+		}
+		winner, _ = play(handsCopy, judgePart2)
+		return winner
+	}
+	return judgePart1(round, hands)
 }
 
 func isOver(hands map[string][]int) (winningHand []int, over bool) {
@@ -220,7 +572,7 @@ func isOver(hands map[string][]int) (winningHand []int, over bool) {
 	return winningHand, true
 }
 
-func score(hand []int) int {
+func calculateScore(hand []int) int {
 	out := 0
 	for i := 1; len(hand)-i >= 0; i++ {
 		out += i * hand[len(hand)-i]
--- 2020/day22/day22_test.go
+++ 2020/day22/day22_test.go
@@ -28,9 +28,9 @@ func TestPart1(t *testing.T) {
 	)
 }
 
-// func TestPart2(t *testing.T) {
-// 	tester.New(t, day22.Part2).Run(
-// 		tester.FromString(testData).Want(0),
-// 		tester.FromFile("input.txt").Want(0),
-// 	)
-// }
+func TestPart2(t *testing.T) {
+	tester.New(t, day22.Part2).Run(
+		tester.FromString(testData).Want(291),
+		tester.FromFile("input.txt").Want(33206),
+	)
+}

8a7c93a988c2bb219b74d349b0fba85e977968e2
Joe Henke <joed.henke@gmail.com>
2022-05-09 11:35:47 -0400

    2020 day 24 part 2

--- 2020/day24/day24.go
+++ 2020/day24/day24.go
@@ -76,11 +76,101 @@ flip. After all of the instructions have been followed, how many tiles are left
 with the black side up?
 */
 func Part1(r io.Reader) (answer int, err error) {
-	return day24(r)
+	locs, err := day24(r)
+	if err != nil {
+		return 0, err
+	}
+	return len(locs), nil
 }
 
+/*
+Part2 Prompt
+
+--- Part Two ---
+The tile floor in the lobby is meant to be a living art exhibit. Every day, the
+tiles are all flipped according to the following rules:
+
+- Any black tile with zero or more than 2 black tiles immediately adjacent to
+it is flipped to white.
+- Any white tile with exactly 2 black tiles immediately adjacent to it is
+flipped to black.
+
+Here, tiles immediately adjacent means the six tiles directly touching the tile
+in question.
+
+The rules are applied simultaneously to every tile; put another way, it is
+first determined which tiles need to be flipped, then they are all flipped at
+the same time.
+
+In the above example, the number of black tiles that are facing up after the
+given number of days has passed is as follows:
+
+    Day 1: 15
+    Day 2: 12
+    Day 3: 25
+    Day 4: 14
+    Day 5: 23
+    Day 6: 28
+    Day 7: 41
+    Day 8: 37
+    Day 9: 49
+    Day 10: 37
+
+    Day 20: 132
+    Day 30: 259
+    Day 40: 406
+    Day 50: 566
+    Day 60: 788
+    Day 70: 1106
+    Day 80: 1373
+    Day 90: 1844
+    Day 100: 2208
+
+After executing this process a total of 100 times, there would be 2208 black
+tiles facing up.
+
+How many tiles will be black after 100 days?
+*/
 func Part2(r io.Reader) (answer int, err error) {
-	return day24(r)
+	// initialize using part 1
+	locs, err := day24(r)
+	if err != nil {
+		return 0, err
+	}
+
+	// step through 100 days of rules from part 2
+	for i := 0; i < 100; i++ {
+		// get bounding box of known black tiles at this point
+		var minX, maxX, minY, maxY int
+		for l := range locs {
+			updateBounds(&minX, &maxX, l.x)
+			updateBounds(&minY, &maxY, l.y)
+		}
+		toFlip := make(map[loc]bool)
+
+		// check all tiles that may be adjacent to any black tile
+		for x := minX - 2; x <= maxX+2; x++ {
+			for y := minY - 1; y <= maxY+1; y++ {
+				n := neighbors(locs, x, y)
+				l := loc{x, y}
+				if _, ok := locs[l]; ok && (n == 0 || n > 2) {
+					toFlip[l] = false
+				} else if !ok && (n == 2) {
+					toFlip[l] = true
+				}
+			}
+		}
+
+		// flip them after calculating them at all at once
+		for l, v := range toFlip {
+			if v {
+				locs[l] = struct{}{}
+			} else {
+				delete(locs, l)
+			}
+		}
+	}
+	return len(locs), nil
 }
 
 type loc struct {
@@ -91,27 +181,6 @@ type change struct {
 	dx, dy int
 }
 
-func day24(r io.Reader) (answer int, err error) {
-	locs := make(map[loc]bool)
-	if err := input.ForEachLine(r, func(line string) error {
-		x, y := 0, 0
-		forEachInstruction(line, func(delta change) {
-			x += delta.dx
-			y += delta.dy
-		})
-		l := loc{x, y}
-		if locs[l] {
-			delete(locs, l)
-		} else {
-			locs[l] = true
-		}
-		return nil
-	}); err != nil {
-		return 0, err
-	}
-	return len(locs), nil
-}
-
 var instructions = map[string]change{
 	"e":  {2, 0},
 	"se": {1, -1},
@@ -119,6 +188,46 @@ var instructions = map[string]change{
 	"w":  {-2, 0},
 	"nw": {-1, 1},
 	"ne": {1, 1},
+}
+
+func updateBounds(min *int, max *int, x int) {
+	if x < *min {
+		*min = x
+	}
+	if x > *max {
+		*max = x
+	}
+}
+
+func neighbors(locs map[loc]struct{}, x, y int) int {
+	n := 0
+	for _, delta := range instructions {
+		if _, ok := locs[loc{x + delta.dx, y + delta.dy}]; ok {
+			n++
+		}
+	}
+	return n
+}
+
+func day24(r io.Reader) (locs map[loc]struct{}, err error) {
+	locs = make(map[loc]struct{})
+	if err := input.ForEachLine(r, func(line string) error {
+		x, y := 0, 0
+		forEachInstruction(line, func(delta change) {
+			x += delta.dx
+			y += delta.dy
+		})
+		l := loc{x, y}
+		if _, ok := locs[l]; ok {
+			delete(locs, l)
+		} else {
+			locs[l] = struct{}{}
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return locs, nil
 }
 
 func forEachInstruction(line string, f func(delta change)) {

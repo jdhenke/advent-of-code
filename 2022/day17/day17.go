d92cb22095c97599956d400447afb807cd42a7e3
Joe Henke <joed.henke@gmail.com>
2023-09-23 09:59:03 -0400

    Ensure docs 2022 day 17 part 2

--- 2022/day17/day17.go
+++ 2022/day17/day17.go
@@ -378,6 +378,18 @@ func Part1(r io.Reader) (answer int, err error) {
 	return day17(r)
 }
 
+/*
+Part2 Prompt
+
+--- Part Two ---
+The elephants are not impressed by your simulation. They demand to know how
+tall the tower will be after 1000000000000 rocks have stopped! Only then will
+they feel confident enough to proceed through the cave.
+
+In the example above, the tower would be 1514285714288 units tall!
+
+How tall will the tower be after 1000000000000 rocks have stopped?
+*/
 func Part2(r io.Reader) (answer int, err error) {
 	return day17(r)
 }
